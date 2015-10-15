9683bc8e4d5a51e2fc4bdbe77d648c63c78fe3fe
Brianbrifri <Brianbrifri@gmail.com>
2015-10-15 02:25:08 -0500

    set up for IDE

---
+++ src/Board.java
@@ -0,0 +1,170 @@
+/* 
+ * Project 5
+ * This is a minesweeper program with icons
+ * that uses recursion
+ */
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.*;
+
+
+@SuppressWarnings("serial")
+public class Board extends JFrame {
+	
+	//Create 2 JButtons, start and show as well as an 8x8
+	//2D array of cells named cellButton
+	JButton start = new JButton("Start");
+	JButton show = new JButton("Show");
+	Cell cellButton[][] = new Cell[8][8];
+	
+	//Constructor that creates panels
+	public Board() {
+		
+		//minefield panel with an 8x8 grid layout
+		JPanel mineField = new JPanel();    
+		mineField.setLayout(new GridLayout(8, 8));
+		
+		//Create instance of listener from listener class
+		ListenerClass listener = new ListenerClass();
+		
+		//Add cellButton/cells to the panel and add the listener at same time
+		for (int i = 0; i < 8; i++) {
+			for(int j = 0; j < 8; j++) {
+				cellButton[i][j] = new Cell();
+				cellButton[i][j].addActionListener(listener);
+				mineField.add(cellButton[i][j]); 
+			}
+		}    
+	   
+		//Create a boarderpanel and top panel. Top Panel holds the 
+		//show and start cellButton and border panel holds top panel
+		//and minefield
+		JPanel boarderPanel = new JPanel(new BorderLayout());  
+		JPanel topPanel = new JPanel(new GridLayout(1, 2));
+		
+		//Add start and show cellButton to top panel
+		topPanel.add(start, BorderLayout.NORTH); 
+		topPanel.add(show, BorderLayout.NORTH); 
+		
+		//Add listeners to start and stop cellButton
+		start.addActionListener(listener);
+		show.addActionListener(listener);
+		
+		//Add top panel and minefield to boarder panel
+		boarderPanel.add(topPanel, BorderLayout.NORTH);    
+		boarderPanel.add(mineField, BorderLayout.CENTER);
+		
+		//Add boarder panel into the frame    
+		add(boarderPanel, BorderLayout.CENTER);    
+	}
+	
+	//Main method
+	public static void main(String[] args) {    
+		Board frame = new Board();    
+		frame.setTitle("Mine Sweeper");    
+		frame.setSize(500, 500);    
+		frame.setLocationRelativeTo(null);  
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    
+		frame.setVisible(true);  
+	}
+		
+	
+	//Expand function with recursion
+	//This part checks all cells around for mines and adds them into sum
+	public void expand(int row, int column) {
+		int sum = 0;
+		for(int i = row - 1; i <= row + 1; i++) {
+			for(int j = column - 1; j <= column + 1; j++) {
+				if(!(row == i && column == j) && i >= 0 && j >= 0 && i < cellButton.length && j < cellButton[0].length) {
+					if(cellButton[i][j].getIsMine()) {
+						sum++;
+					}
+				}
+			}
+		}
+		
+		//Calls setNumberIcon on the array location and sets image
+		//based on how many mines are around
+		cellButton[row][column].setNumberIcon(sum);
+		
+		//If there are no mines around, expand again and check around
+		if(sum == 0) {
+			for (int i = row -1; i <= row + 1; i++) {
+				for(int j = column - 1; j <= column + 1; j++) {
+					if(!(row == i && column == j) && i >= 0 && j >= 0 && i < cellButton.length && j < cellButton[0].length) {
+						try {
+							//If the button is not a mine and the cell has not been cleared, call expand again
+							if(!cellButton[i][j].getIsMine() && cellButton[i][j].getTheIcon() != null) {
+								expand(i, j);
+							}
+						}
+						catch(ArrayIndexOutOfBoundsException ex) {						
+						}
+					}
+				}
+			}
+		}	
+	}
+	
+	//Listener that detects what button is clicked then acts based on that
+	class ListenerClass implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			//If the show button is clicked
+			if(e.getSource() == show) {
+				for(int i = 0; i < 8; i++) {
+					for(int j = 0; j < 8; j++) {
+						//If the button is a mine, call showWhereMinesAre method
+						//that sets icons to flags
+						if(cellButton[i][j].getIsMine()) {
+							cellButton[i][j].showWhereMinesAre();
+						}
+					}
+				}	
+			}
+			
+			//If start button is clicked
+			else if(e.getSource() == start) {
+				Random rand = new Random();
+				//disable start button for future use
+				start.setEnabled(false);
+				//set random coordinates to mines, if already a mine, decrease counter and try again
+				for(int i = 1; i <= 8; i++) {
+					int x = rand.nextInt(8);
+					int y = rand.nextInt(8);
+					if(!cellButton[x][y].getIsMine()) {
+						cellButton[x][y].setAsMine();
+					}
+					
+					else {
+						i--;
+						continue;
+					}
+				}			
+			}
+			
+			//If a cell button, loop through array to find matching button
+			else  {
+				for (int i = 0; i < cellButton.length; i++) {
+					for(int j = 0; j < cellButton[0].length; j++) {
+						//once match found, if a mine was clicked, call setMineIcon method
+						if (e.getSource() == cellButton[i][j]){
+							if(cellButton[i][j].getIsMine()) {
+								cellButton[i][j].setMineIcon();
+							}
+							//otherwise, expand recursively
+							else {
+								expand(i, j);
+							}							
+						}
+					}
+				}
+			}
+		}	
+	}
+}
+
---
+++ src/Cell.java
@@ -0,0 +1,86 @@
+import javax.swing.*;
+
+
+@SuppressWarnings("serial")
+//Cell class that contains all info to change icons and a boolean mine flag
+public class Cell extends JButton {
+	
+	//Initialize ImageIcon variables and boolean
+	ImageIcon mine, one, two, three, four, five, six, seven, eight, flag, unknown;
+	boolean isMine = false;
+	
+	public Cell() {
+		//Set variables to match with their respective images
+		mine = new ImageIcon(this.getClass().getResource("mine.png"));
+		unknown = new ImageIcon(this.getClass().getResource("unknown.png"));
+		one = new ImageIcon(this.getClass().getResource("1.png"));
+		two = new ImageIcon(this.getClass().getResource("2.png"));
+		three = new ImageIcon(this.getClass().getResource("3.png"));
+		four = new ImageIcon(this.getClass().getResource("4.png"));
+		five = new ImageIcon(this.getClass().getResource("5.png"));
+		six = new ImageIcon(this.getClass().getResource("6.png"));
+		seven = new ImageIcon(this.getClass().getResource("7.png"));
+		eight = new ImageIcon(this.getClass().getResource("8.png"));
+		flag = new ImageIcon(this.getClass().getResource("flag.png"));
+		
+		//Set icon to a question mark upon creation
+		setIcon(unknown);
+	}
+	
+	//Method to set icon to a mine
+	public void setMineIcon() {
+		setIcon(mine);
+	}
+	
+	//Method to set icon to a number based on how many mines are around it
+	public void setNumberIcon(int mines){
+		switch(mines) {
+			case 0: setIcon(null);
+				break;
+			case 1: setIcon(one);
+				break;
+			case 2: setIcon(two);
+				break;
+			case 3: setIcon(three);
+				break;
+			case 4: setIcon(four);
+				break;
+			case 5: setIcon(five);
+				break;
+			case 6: setIcon(six);
+				break;
+			case 7: setIcon(seven);
+				break;
+			case 8: setIcon(eight);
+				break;
+			default: setIcon(null);
+		}
+	}
+	
+	//Method to return icon type. Mainly used to return null
+	public Icon getTheIcon() {
+		return getIcon();
+	}
+	
+	//Method to check if this cell is a mine
+	public boolean getIsMine() {
+		return isMine;
+	}
+	
+	//Turns this cell into a mine
+	public void setAsMine() {
+		isMine = true;
+	}
+	
+	//Shows if this is a mine by turning the icon into a flag
+	//If this cell already shows a mine, skip this action
+	public void showWhereMinesAre() {
+		if(getIcon() == mine) {
+			return;
+		}
+		else {
+			setIcon(flag);
+		}
+	}
+	
+}
